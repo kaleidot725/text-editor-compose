5119ebb50769ee1c35e22cbabc40cb278eef17e4
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-06-27 22:08:38 +0900

    Bugfix decorationBox

--- texteditor/src/main/java/jp/kaleidot725/texteditor/view/TextEditor.kt
+++ texteditor/src/main/java/jp/kaleidot725/texteditor/view/TextEditor.kt
@@ -1,6 +1,7 @@
 package jp.kaleidot725.texteditor.view
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.runtime.Composable
@@ -30,52 +31,61 @@ fun TextEditor(
 ) {
     val isMultipleSelectionMode by textEditorState.isMultipleSelectionMode
 
+    // workaround: prevent to hide ime when editor delete newline
+    val focusManager = LocalFocusManager.current
+
     LazyColumn(modifier = modifier) {
         itemsIndexed(
             items = textEditorState.toEditable().fields,
             key = { _, item -> item.id }
         ) { index, textFieldState ->
-            // workaround: prevent to hide ime when editor delete newline
-            val focusManager = LocalFocusManager.current
+
             val focusRequester by remember { mutableStateOf(FocusRequester()) }
 
             LaunchedEffect(textFieldState.isSelected) {
                 if (textFieldState.isSelected) focusRequester.requestFocus()
             }
 
-            decorationBox(index, textFieldState.isSelected) {
-                TextField(
-                    textFieldValue = textFieldState.value,
-                    enabled = !isMultipleSelectionMode,
-                    onUpdateText = { newText ->
-                        textEditorState.toEditable()
-                            .updateField(targetIndex = index, textFieldValue = newText)
-                        onUpdatedState()
-                    },
-                    onAddNewLine = { newText ->
-                        textEditorState.toEditable()
-                            .splitField(targetIndex = index, textFieldValue = newText)
-                        onUpdatedState()
-                    },
-                    onDeleteNewLine = {
-                        textEditorState.toEditable().deleteField(targetIndex = index)
-                        onUpdatedState()
+            decorationBox(
+                index = index,
+                isSelected = textFieldState.isSelected,
+                innerTextField = { modifier ->
+                    TextField(
+                        textFieldValue = textFieldState.value,
+                        enabled = !isMultipleSelectionMode,
+                        onUpdateText = { newText ->
+                            textEditorState.toEditable()
+                                .updateField(targetIndex = index, textFieldValue = newText)
+                            onUpdatedState()
+                        },
+                        onAddNewLine = { newText ->
+                            textEditorState.toEditable()
+                                .splitField(targetIndex = index, textFieldValue = newText)
+                            onUpdatedState()
+                        },
+                        onDeleteNewLine = {
+                            textEditorState.toEditable().deleteField(targetIndex = index)
+                            onUpdatedState()
 
-                        // workaround: prevent to hide ime when editor delete newline
-                        focusManager.moveFocus(FocusDirection.Up)
-                    },
-                    focusRequester = focusRequester,
-                    onFocus = {
-                        textEditorState.toEditable().selectField(targetIndex = index)
-                        onUpdatedState()
-                    },
-                    modifier = Modifier.clickable {
-                        if (!isMultipleSelectionMode) return@clickable
-                        textEditorState.toEditable().selectField(targetIndex = index)
-                        onUpdatedState()
-                    }
-                )
-            }
+                            // workaround: prevent to hide ime when editor delete newline
+                            focusManager.moveFocus(FocusDirection.Up)
+                        },
+                        focusRequester = focusRequester,
+                        onFocus = {
+                            textEditorState.toEditable().selectField(targetIndex = index)
+                            onUpdatedState()
+                        },
+                        modifier = modifier.clickable(
+                            interactionSource = remember { MutableInteractionSource() },
+                            indication = null
+                        ) {
+                            if (!isMultipleSelectionMode) return@clickable
+                            textEditorState.toEditable().selectField(targetIndex = index)
+                            onUpdatedState()
+                        }
+                    )
+                }
+            )
         }
     }
 }

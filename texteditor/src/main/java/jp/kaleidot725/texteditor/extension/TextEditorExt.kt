985e3051ef87a7d38dc2b8c733cfde8d8bd02a4c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-07-02 14:57:36 +0900

    Add text editor extension

--- texteditor/src/main/java/jp/kaleidot725/texteditor/extension/TextEditorExt.kt
+++ texteditor/src/main/java/jp/kaleidot725/texteditor/extension/TextEditorExt.kt
@@ -3,13 +3,23 @@ package jp.kaleidot725.texteditor.extension
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import jp.kaleidot725.texteditor.controller.TextEditorController
 import jp.kaleidot725.texteditor.factory.TextEditorStateFactory
 
 @Composable
-fun rememberTextEditorController(lines: List<String>, onChanged: () -> Unit) = remember {
+fun rememberTextEditorController(text: String, onChanged: (controller: TextEditorController) -> Unit) = remember {
+    mutableStateOf(
+        TextEditorStateFactory.create(text).apply {
+            setOnChangedTextListener { onChanged(this) }
+        }
+    )
+}
+
+@Composable
+fun rememberTextEditorController(lines: List<String>, onChanged: (controller: TextEditorController) -> Unit) = remember {
     mutableStateOf(
         TextEditorStateFactory.create(lines).apply {
-            setOnChangedTextListener(onChanged)
+            setOnChangedTextListener { onChanged(this) }
         }
     )
 }
